c7fbde104bb54e44616af6f950b6507993762378
Abdulrhman-elmofty <bodyelmofty1@gmail.com>
2026-08-16 15:32:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from groq import Groq
 
 st.title("Summarize")
 
-#client = Groq(api_key=st.secrets["Groq_Api_Key"])
+client = Groq(api_key=st.secrets['Groq_Api_Key'])
 
 files = st.file_uploader("upload your pdf", type=["pdf"]) 
 
@@ -25,3 +25,19 @@ if files is not None:
 else:
     text_from_pdf = ""
 text_area = st.text_area("Or paste your text here:", height=200, value=text_from_pdf)
+
+if st.button("Summarize!"):
+    if len(text_area.split()) < 10:
+        st.warning("Your text is too short")
+    else:
+        with st.spinner("Summarizing..."):
+            arabic_letters = sum(1 for c in text_area if '\u0600' <= c <= '\u06ff')
+            lang = "Arabic" if arabic_letters > 10 else "English"
+            response = client.chat.completions.create(
+                model = "llama-3.3-70b-versalite",
+                messages = [
+                    {"role" : "system" , "content" : f"you are a helpful assistant, summarize the text into 4 simple sentences, in {lang} language"}
+                    {"role" : "user" , "content" : text}
+                ]
+            )
+            st.success(response.choices[0].message.content)
